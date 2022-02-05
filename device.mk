1c2fc942096cf68abd3c59e7e3584fefe579c397
Foxtrot47 <jjneutron@outlook.com>
2022-02-05 10:24:18 +0100

    vayu: Switch to common QTI WFD Component
    co-authored-by: clhex <clhexftw@protonmail.com>
    Signed-off-by: clhex <clhexftw@protonmail.com>

--- device.mk
+++ device.mk
@@ -302,6 +302,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     bt \
     usb \
     telephony \
+    wfd \
     wlan
 
 # RIL
@@ -397,5 +398,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libwfdaac_vendor
 
 include vendor/xiaomi/vayu/vayu-vendor.mk
