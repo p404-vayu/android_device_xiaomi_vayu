1d01082b31788d3d30a86cb27fd4a61f95101d43
clhex <clhexftw@protonmail.com>
2022-07-20 19:53:26 +0530

    vayu: Switch to Common QTI vibrator component

--- device.mk
+++ device.mk
@@ -363,6 +363,7 @@ TARGET_BOARD_PLATFORM := msmnile
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
     display \
+    vibrator \
     perf \
     bt \
     usb \
@@ -454,13 +455,6 @@ PRODUCT_COPY_FILES += \
 # Verified boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Vndservicemanager
 PRODUCT_PACKAGES += \
