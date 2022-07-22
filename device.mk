fa50efbbb6eaf8e3766471567d226be0367f1dd7
xenxynon <flynryder427@gmail.com>
2022-07-22 15:26:02 +0530

    vayu: Drop mock hidl power stats
    * E android.hardware.power.stats@1.0-service-mock Failed to getEnergyData

--- device.mk
+++ device.mk
@@ -374,8 +374,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
