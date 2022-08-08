4416058f6e0241db088d36634cef0ff58ff38884
OmarAlCoptan <omarcoptan9@gmail.com>
2022-08-08 22:07:14 +0200

    vayu: use proton clang
     * git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton-clang
    
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>
    Signed-off-by: clhex <clhexftw@protonmail.com>
    Change-Id: I7934fc9b21018a464f7a1667e3d71b93a16d6674

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,8 @@ TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_CONFIG := vayu_user_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
