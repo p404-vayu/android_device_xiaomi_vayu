305e37867713b418784bf2ae211ab83d27195982
clhex <clhexftw@protonmail.com>
2022-08-16 19:11:14 +0530

    vayu: Switch to Common QTI Init Component
    Change-Id: I70c6f3cf13592f1c4294ab1cea88415d15060bbd

--- device.mk
+++ device.mk
@@ -171,20 +171,8 @@ PRODUCT_COPY_FILES += \
 
 # Init scripts
 PRODUCT_PACKAGES += \
-    init.mi.usb.sh \
-    init.qcom.post_boot.sh \
-    init.qcom.sensors.sh \
-    init.qcom.sh \
-    init.qcom.usb.sh \
-    init.qti.dcvs.sh
-
-PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.qcom.rc \
-    init.qcom.usb.rc \
-    init.recovery.qcom.rc \
-    init.target.rc \
-    ueventd.qcom.rc
+    init.target.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
@@ -284,6 +272,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     nq-nfc \
     overlay \
     av \
+    init \
     perf \
     bt \
     usb \
