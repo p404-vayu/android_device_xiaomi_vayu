c970bef3640fa20791a076e9fb9ad4947862e76b
clhex <clhexftw@protonmail.com>
2022-10-13 01:03:50 -0400

    vayu: Bringup Project404

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vayu.mk
+    $(LOCAL_DIR)/p404_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vayu-eng \
-    arrow_vayu-userdebug \
-    arrow_vayu-user
+    p404_vayu-eng \
+    p404_vayu-userdebug \
+    p404_vayu-user
--- p404_vayu.mk
+++ p404_vayu.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common Project404 configurations
+$(call inherit-product, vendor/404/configs/common.mk)
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := p404_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -28,6 +28,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=M2102J20SI
 
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:11/RKQ1.200826.002/V12.5.7.0.RJUMIXM:user/release-keys
-
-DEVICE_MAINTAINER := kubersharma001
-
