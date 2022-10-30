clhexdumpyard/android_device_xiaomi_vayu-1

vayu: separate out zip name
* for gapps and vanilla build
* there is no separation for hence it's sort of confusing of figure out them

Signed-off-by: xenxynon <flynryder427@gmail.com>

p404_vayu.mk
@@ -30,4 +30,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.3.0.SJUMIXM:user/release-keys
 
 # P404 stuffs
-P404_BUILDTYPE := SHINKA
+ifeq ($(WITH_GAPPS),true)
+P404_BUILDTYPE := SHINKA-GAPPS
+else
+P404_BUILDTYPE := SHINKA-VANILLA
+endif

p404-vayu/android_device_xiaomi_vayu

Change-Id: I258d2f08cc8dab2510080b4a6cb2fba56c0834ce
Signed-off-by: clhex <clhexftw@protonmail.com>

p404_vayu.mk
@@ -30,4 +30,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys
 
 # P404 stuffs
-P404_BUILDTYPE := Tokui
+ifeq ($(WITH_GAPPS),true)
+P404_BUILDTYPE := Tokui-GAPPS
+else
+P404_BUILDTYPE := Tokui-VANILLA
+endif