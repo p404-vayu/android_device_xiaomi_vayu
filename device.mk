c7601eed67d984311736e139d8a590f7cd35b7fb
clhex <clhexftw@protonmail.com>
2023-01-04 11:41:10 -0500

    Revert "vayu: Switch to common QTI Audio component"

--- device.mk
+++ device.mk
@@ -32,31 +32,160 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.2-impl \
+    audio.primary.msmnile \
+    audio.r_submix.default \
+    audio.usb.default \
+    liba2dpoffload \
+    libaudiopreprocessing \
+    libbatterylistener \
+    libbundlewrapper \
+    libcomprcapture \
+    libdownmix \
+    libdynproc \
+    libeffectproxy \
+    libexthwplugin \
+    libhdmiedid \
+    libhfp \
+    libldnhncr \
+    libqcompostprocbundle \
+    libreverbwrapper \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libsndmonitor \
+    libspkrprot \
+    libvisualizer \
+    libvolumelistener
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.min.duration.secs=30 \
-    audio.offload.video=true \
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+
+PRODUCT_VENDOR_PROPERTIES += \
+    aaudio.hw_burst_min_usec=2000 \
+    aaudio.mmap_exclusive_policy=2 \
+    aaudio.mmap_policy=2 \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.tmic.enabled=false \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicecomm=true \
+    persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.hifi=false \
+    persist.vendor.audio.ras.enabled=false \
     persist.vendor.audio.ring.filter.mask=0 \
-    ro.vendor.audio.soundfx.usb=true \
-    ro.af.client_heap_size_kbyte=7168 \
+    persist.vendor.audio.voicecall.speaker.stereo=true \
+    ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=11 \
+    ro.vendor.audio.game.effect=true \
+    ro.vendor.audio.hifi=true \
+    ro.vendor.audio.ring.filter=true \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.ssr=false \
+    ro.vendor.audio.sfx.earadj=true \
+    ro.vendor.audio.sos=true \
+    ro.vendor.audio.soundfx.type=mi \
+    ro.vendor.audio.soundfx.usb=true \
+    ro.vendor.audio.us.proximity=false \
+    ro.vendor.audio.voice.change.support=true \
+    ro.vendor.audio.voice.volume.boost=manual \
+    vendor.audio.adm.buffering.ms=6 \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.enable.mirrorlink=false \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    vendor.audio.hal.boot.timeout.ms=20000 \
+    vendor.audio.hal.output.suspend.supported=false \
+    vendor.audio.hw.aac.encoder=false \
+    vendor.audio.offload.buffer.size.kb=32 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    vendor.audio.offload.track.enable=false \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.safx.pbe.enabled=false \
+    vendor.audio.spkcal.copy.inhal=true \
+    vendor.audio.tunnel.encode=false \
     vendor.audio.usb.disable.sidetone=true \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio.volume.headset.gain.depcal=true \
+    vendor.audio_hal.in_period_size=144 \
+    vendor.audio_hal.period_multiplier=3 \
+    vendor.audio_hal.period_size=192 \
+    vendor.voice.path.for.pcm.voip=false \
+
+PRODUCT_ODM_PROPERTIES += \
+    vendor.audio.feature.a2dp_offload.enable=true \
+    vendor.audio.feature.afe_proxy.enable=true \
+    vendor.audio.feature.anc_headset.enable=true \
+    vendor.audio.feature.battery_listener.enable=true \
+    vendor.audio.feature.compr_cap.enable=false \
+    vendor.audio.feature.compress_in.enable=false \
+    vendor.audio.feature.compress_meta_data.enable=true \
+    vendor.audio.feature.compr_voip.enable=false \
+    vendor.audio.feature.concurrent_capture.enable=false \
+    vendor.audio.feature.custom_stereo.enable=true \
+    vendor.audio.feature.display_port.enable=true \
+    vendor.audio.feature.dsm_feedback.enable=false \
+    vendor.audio.feature.dynamic_ecns.enable=false \
+    vendor.audio.feature.ext_hw_plugin.enable=true \
+    vendor.audio.feature.external_dsp.enable=false \
+    vendor.audio.feature.external_speaker.enable=false \
+    vendor.audio.feature.external_speaker_tfa.enable=false \
+    vendor.audio.feature.fluence.enable=true \
+    vendor.audio.feature.fm.enable=true \
+    vendor.audio.feature.hdmi_edid.enable=true \
+    vendor.audio.feature.hdmi_passthrough.enable=true \
+    vendor.audio.feature.hfp.enable=true \
+    vendor.audio.feature.hifi_audio.enable=false \
+    vendor.audio.feature.hwdep_cal.enable=false \
+    vendor.audio.feature.incall_music.enable=true \
+    vendor.audio.feature.multi_voice_session.enable=true \
+    vendor.audio.feature.keep_alive.enable=true \
+    vendor.audio.feature.kpi_optimize.enable=true \
+    vendor.audio.feature.maxx_audio.enable=false \
+    vendor.audio.feature.ras.enable=true \
+    vendor.audio.feature.record_play_concurency.enable=false \
+    vendor.audio.feature.src_trkn.enable=true \
+    vendor.audio.feature.spkr_prot.enable=true \
+    vendor.audio.feature.ssrec.enable=true \
+    vendor.audio.feature.usb_offload.enable=true \
+    vendor.audio.feature.usb_offload_burst_mode.enable=true \
+    vendor.audio.feature.usb_offload_sidetone_volume.enable=false \
+    vendor.audio.feature.deepbuffer_as_primary.enable=false \
+    vendor.audio.feature.vbat.enable=true \
+    vendor.audio.feature.wsa.enable=false \
+    vendor.audio.feature.audiozoom.enable=false \
+    vendor.audio.feature.snd_mon.enable=true \
 
 PRODUCT_PRODUCT_PROPERTY += \
-    ro.vendor.audio.sdk.fluencetype=fluence
-
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml
-
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    audio.sys.noisy.broadcast.delay=500 \
+    audio.sys.mute.latency.factor=2 \
+    audio.sys.routing.latency=0 \
+    audio.offload.min.duration.secs=30 \
+    audio.sys.offload.pstimeout.secs=3
 
 # Bluetooth
 PRODUCT_VENDOR_PROPERTIES += \
+    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
+    persist.bluetooth.a2dp_offload.disabled=false \
+    persist.vendor.bt.aac_frm_ctl.enabled=false \
     persist.vendor.btsatck.absvolfeature=true \
     persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true \
     persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false \
@@ -279,6 +408,36 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# Listen
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.audio.soundtrigger=sva \
+    ro.vendor.audio.soundtrigger.cnn.adsp.level=27 \
+    ro.vendor.audio.soundtrigger.cnn.level=27 \
+    ro.vendor.audio.soundtrigger.gmm.adsp.level=50 \
+    ro.vendor.audio.soundtrigger.gmm.level=50 \
+    ro.vendor.audio.soundtrigger.gmm.user.adsp.level=10 \
+    ro.vendor.audio.soundtrigger.gmm.user.level=10 \
+    ro.vendor.audio.soundtrigger.lowpower=true \
+    ro.vendor.audio.soundtrigger.training.level=50 \
+    ro.vendor.audio.soundtrigger.vop.adsp.level=10 \
+    ro.vendor.audio.soundtrigger.vop.level=10 \
+    ro.vendor.audio.soundtrigger.xanzn.cnn.level=25 \
+    ro.vendor.audio.soundtrigger.xanzn.gmm.level=45 \
+    ro.vendor.audio.soundtrigger.xanzn.gmm.user.level=50 \
+    ro.vendor.audio.soundtrigger.xanzn.vop.level=20 \
+    ro.vendor.audio.soundtrigger.xatx.cnn.level.high=27 \
+    ro.vendor.audio.soundtrigger.xatx.cnn.level.low=27 \
+    ro.vendor.audio.soundtrigger.xatx.cnn.level.medium=27 \
+    ro.vendor.audio.soundtrigger.xatx.gmm.level.high=50 \
+    ro.vendor.audio.soundtrigger.xatx.gmm.level.low=50 \
+    ro.vendor.audio.soundtrigger.xatx.gmm.level.medium=50 \
+    ro.vendor.audio.soundtrigger.xatx.gmm.user.level.high=10 \
+    ro.vendor.audio.soundtrigger.xatx.gmm.user.level.low=10 \
+    ro.vendor.audio.soundtrigger.xatx.gmm.user.level.medium=10 \
+    ro.vendor.audio.soundtrigger.xatx.vop.level.high=10 \
+    ro.vendor.audio.soundtrigger.xatx.vop.level.low=10 \
+    ro.vendor.audio.soundtrigger.xatx.vop.level.medium=10 \
+
 # Miui Camera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicam/miuicam-vendor.mk)
 
@@ -312,7 +471,6 @@ TARGET_BOARD_PLATFORM := msmnile
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
-    audio \
     adreno \
     display \
     gps \
